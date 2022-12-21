d8e5fa426c48c26015a4f5276029faa9198dad97
Aswin p <aswinp910@gmail.com>
2022-12-21 19:55:40 +0530

    first change

---
+++ basics.js
@@ -0,0 +1,87 @@
+// //shift operators
+
+// console.log(5>>1);
+
+//conditional statements
+// var a=10;
+// var b=10;
+
+// if (a>b) {
+
+//     console.log("a greater than b")
+// } 
+// else if (a==b) {
+//     console.log("a is equal to  b")
+    
+// }
+// else
+//  {
+//     console.log("a is not lessthan b")
+
+
+// }
+
+// var t=0;
+// if(t){
+//     console.log('t has value');
+// }
+// else{
+//     console.log('t is zero');
+// }
+
+
+//Looping statements
+// for (let i = 0; i < 10; i++)
+//  {
+//     console.log(i+1);
+    
+// // }
+// var array=[1,2,3,4];
+// for(let i=0;i<4;i++){
+//     console.log(array[i]);
+// }
+//for...of loop
+
+// for (const i of array) {
+//     console.log(i);
+    
+// }
+
+// //for...in loop
+
+// var obj={name:'aswin',age:"21"}
+// for (const i in obj){
+//     console.log(obj[i]);
+// }
+
+
+
+//while loop
+// var i=0;
+
+// while (i<10) 
+// {
+// console.log(i+1);
+// i++;
+    
+// }
+
+// var array=[9,8,7,6,5,4,3,2,1]
+// while(i<5)
+// {
+//     console.log(array[i]);
+// }
+
+//Do while loop
+// do {
+//     console.log(i+1);
+//     i++
+// } while (i<10);
+
+var array=[1,2,3,4];
+var i=0;
+do {
+    console.log(array[i]);
+    i++
+
+} while (i<4);
---
+++ function1.js
@@ -0,0 +1,14 @@
+//   function convertfahrenheit(celsius)
+//   {
+
+//  let temp=celsius
+//  let result=(temp*9/5)+32
+//   console.log('fahrenheit value:',result)
+    
+//   }
+
+// convertfahrenheit(100);
+
+function test (){
+  console.log("hello");
+}as
